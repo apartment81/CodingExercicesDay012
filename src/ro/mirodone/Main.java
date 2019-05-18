95b787abdbe012bf1e2006d838e8d59b9bc8a279
apartment81 <bogdan.mirodone@gmail.com>
2019-05-19 01:00:10 +0300

    done

--- src/ro/mirodone/Main.java
+++ src/ro/mirodone/Main.java
@@ -1,12 +1,33 @@
 package ro.mirodone;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Student student1 = new Student();
-       // Student s2 = new Student();
+/*        Student student1 = new Student();
         student1.enrollCourse();
+        student1.payTuition();
+        System.out.println(student1.showInfo());*/
+
+        System.out.print("Enter the number of new students to enroll: ");
+        Scanner scanner = new Scanner(System.in);
+        int numOfStudents = scanner.nextInt();
+
+
+        // create an array to add students
+        Student[] students = new Student[numOfStudents];
+        for (int i = 0; i < numOfStudents; i++) {
+            students[i] = new Student();
+            students[i].enrollCourse();
+            students[i].payTuition();
+        }
+
+        for (int i = 0; i < numOfStudents; i++) {
+            System.out.println(students[i].showInfo());
+        }
+
 
     }
 }
--- src/ro/mirodone/Student.java
+++ src/ro/mirodone/Student.java
@@ -10,55 +10,69 @@ public class Student {
     private String lastName;
     private int gradeYear;
     private String studentId;
-    private String courses;
+    private String courses = "";
     private int tuitionBalance;
-    private int priceOfCourse = 600;
 
-    public Student() {
+    Student() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter student first name: ");
+        System.out.print("Enter student first name: ");
         this.firstName = in.nextLine();
 
-        System.out.println("Enter student last name: ");
+        System.out.print("Enter student last name: ");
         this.lastName = in.nextLine();
 
         System.out.println("Enter student grade year:\n 1 for 9th grade\n 2 for 10th grade\n 3 for 11th grade\n 4 for 12th grade");
         this.gradeYear = in.nextInt();
 
         setStudentId();
-        System.out.println(firstName + " - " + lastName + " - " + gradeYear + " - " + studentId);
-
-
     }
+
     // Generate teh ID
     private void setStudentId() {
         //student id must be 5 digit and grade level must be first digit
-
         id++;
         this.studentId = gradeYear + "" + id;
 
     }
-
     //Enroll courses
 
-    public void enrollCourse () {
-
-
-        do{
-            System.out.println("Enter course to enroll ( Type Q to quit): ");
-            Scanner input = new Scanner(System.in) ;
+    public void enrollCourse() {
+        int priceOfCourse = 600;
+        do {
+            System.out.print("Enter course to enroll ( Type Q to quit): ");
+            Scanner input = new Scanner(System.in);
             String course = input.nextLine();
 
-            if(!course.equalsIgnoreCase("q")){
-                courses = courses + "\n" + course;
-                tuitionBalance = tuitionBalance+ priceOfCourse;
-            }else {
+            if (!course.equalsIgnoreCase("q")) {
+                courses = courses + "\n " + course;
+                tuitionBalance = tuitionBalance + priceOfCourse;
+            } else {
                 break;
             }
-        }while (true);
+        } while (true);
+        viewBalance();
+    }
 
-        System.out.println("Enrolled in :" + courses);
-        System.out.println("Tuition Balance: " + tuitionBalance);
+    private void viewBalance() {
+        System.out.println("Your balance is: $" + tuitionBalance);
+    }
 
+    public void payTuition() {
+
+        System.out.println("Enter the amount you pay:");
+        Scanner input = new Scanner(System.in);
+        int payment = input.nextInt();
+
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of: $" + payment);
+        viewBalance();
+    }
+
+    public String showInfo() {
+        return "Name: " + firstName + " " + lastName
+                + "\nStudent ID: " + studentId
+                + "\nGrade Level: " + gradeYear
+                + "\nCourses enrolled:" + courses
+                + "\nBalance: $" + tuitionBalance;
     }
 }
